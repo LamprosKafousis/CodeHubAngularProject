ed5420a567799088b517329ea95f17c8e063d800
Kafousis <lampros.kafousis@accenture.com>
2019-12-03 18:32:20 +0200

    #service call

--- src/app/list-of-bugs/list-of-bugs.module.ts
+++ src/app/list-of-bugs/list-of-bugs.module.ts
@@ -3,11 +3,11 @@ import { CommonModule } from '@angular/common';
 import { ListOfBugsComponent } from './list-of-bugs/list-of-bugs.component';
 
 
-
 @NgModule({
   declarations: [ListOfBugsComponent],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [ListOfBugsComponent]
 })
 export class ListOfBugsModule { }
--- src/app/services/get-bugs-sorting-paging.service.ts
+++ src/app/services/get-bugs-sorting-paging.service.ts
@@ -1,9 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GetBugsSortingPagingService {
 
-  constructor() { }
+  //private readonly endpoint = 'https://bug-report-system-server.herokuapp.com/bugs?sort=title,desc&page=0&size=10&title=bug&priority=1&reporter=QA&status=Done';
+  private endpoint = 'https://bug-report-system-server.herokuapp.com/bugs?';
+
+  constructor(private http: HttpClient) { }
+
+  getBugsSortingPaging(sort?: string,
+                       page?: number,
+                       size?: number,
+                       title?: string,
+                       priority?: number,
+                       reporter?: string,
+                       status?: string): Observable<any> {
+    if (sort !== '' && sort != null) {
+      this.endpoint = this.endpoint + 'sort=' + sort;
+    }
+
+    return this.http.get(this.endpoint);
+   }
 }
