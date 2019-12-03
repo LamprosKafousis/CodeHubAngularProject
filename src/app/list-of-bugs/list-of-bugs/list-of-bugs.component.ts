c097b4ac8e663b4cb9c41c92c80820f5fb2cb25a
oxana-l7 <58442303+oxana-l7@users.noreply.github.com>
2019-12-04 00:08:43 +0200

    basic list

--- src/app/list-of-bugs/list-of-bugs/list-of-bugs.component.ts
+++ src/app/list-of-bugs/list-of-bugs/list-of-bugs.component.ts
@@ -7,17 +7,35 @@ import { GetBugsSortingPagingService } from 'src/app/services/get-bugs-sorting-p
   templateUrl: './list-of-bugs.component.html',
   styleUrls: ['./list-of-bugs.component.scss']
 })
+
 export class ListOfBugsComponent implements OnInit {
 
   constructor(private getBugsService: GetBugsService,
               private getBugsSortingPagingService: GetBugsSortingPagingService) { }
-  // bug: Array<string>;
-
+  public bugs: Bug[];
+  headElements = ['       TITLE       ', 'PRIORITY', 'REPORTER', '  DATE CREATED  ', '   STATUS  '];
   ngOnInit() {
-    this.getBugsService.getBugs().subscribe((data) => {console.log(data); }) ;
-    this.getBugsSortingPagingService.getBugsSortingPaging().subscribe((data) => {console.log(data)}) ;
-  }
+
+    this.getBugsService.getBugs().subscribe((data) => {data.map((it) => console.log(it.status + it.title)) ;}) ;
+    this.getBugsSortingPagingService.getBugsSortingPaging().subscribe((bugs: Bug[]) => { for(let bug of bugs) { console.log(bug.priority);} this.bugs = bugs; for(let bug of this.bugs) {console.log(bug.title);}});
+    this.getBugsSortingPagingService.getBugsSortingPaging().subscribe((bugs: Bug[]) => {this.bugs = bugs; for(let bug of this.bugs) {console.log(bug.reporter);}});
+
+
+
+
+
   // this.getBugsSortedService.getBugsSorted().subscribe((data) => {console.log(data)}) ;
 }
+}
+interface Bug {
+  title: string;
+  priority: number;
+  reporter: string;
+  createdAt: Date;
+  status: string;
+}
+
+
+
 
 
