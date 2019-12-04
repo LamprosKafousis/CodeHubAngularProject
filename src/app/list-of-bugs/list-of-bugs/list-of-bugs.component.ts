d9fd584dfc9f9a55275d81b0af2771e60cdd8803
Kafousis <lampros.kafousis@accenture.com>
2019-12-04 17:51:44 +0200

    #list of bugs and sorting creation

--- src/app/list-of-bugs/list-of-bugs/list-of-bugs.component.ts
+++ src/app/list-of-bugs/list-of-bugs/list-of-bugs.component.ts
@@ -1,23 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { GetBugsService } from 'src/app/services/get-bugs.service';
-import { GetBugsSortingPagingService } from 'src/app/services/get-bugs-sorting-paging.service';
-
 @Component({
   selector: 'app-list-of-bugs',
   templateUrl: './list-of-bugs.component.html',
   styleUrls: ['./list-of-bugs.component.scss']
 })
+
 export class ListOfBugsComponent implements OnInit {
 
-  constructor(private getBugsService: GetBugsService,
-              private getBugsSortingPagingService: GetBugsSortingPagingService) { }
-  // bug: Array<string>;
-
+  constructor(private getBugsService: GetBugsService) { }
+  public bugs: Bug[];
+  headElements = ['title', 'priority', 'reporter', 'createdAt', 'status'];
   ngOnInit() {
-    this.getBugsService.getBugs().subscribe((data) => {console.log(data); }) ;
-    this.getBugsSortingPagingService.getBugsSortingPaging().subscribe((data) => {console.log(data)}) ;
-  }
-  // this.getBugsSortedService.getBugsSorted().subscribe((data) => {console.log(data)}) ;
+
+  this.getBugsService.getBugs(null, null, null, null, null, null, null, null).subscribe(
+    (bugs: Bug[]) => {this.bugs = bugs;});
 }
+
+sorting( sort: string) {
+  this.getBugsService.getBugs(sort, null, null, null, null, null, null, null).subscribe(
+    (bugs: Bug[]) => {this.bugs = bugs;});
+
+}
+
+}
+interface Bug {
+  title: string;
+  priority: number;
+  reporter: string;
+  createdAt: Date;
+  status: string;
+}
+
+
+
 
 
