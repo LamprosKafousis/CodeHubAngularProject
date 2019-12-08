d273ce512959109f84026d478d675195ec5c084d
oxana-l7 <58442303+oxana-l7@users.noreply.github.com>
2019-12-09 00:42:25 +0200

    added pagination

--- src/app/bugs-management/bugs-management/list/list.component.ts
+++ src/app/bugs-management/bugs-management/list/list.component.ts
@@ -11,14 +11,22 @@ export class ListComponent implements OnInit {
   constructor(private getBugsService: GetBugsService) { }
   public bugs: Bug[];
   headElements = ['title', 'priority', 'reporter', 'createdAt', 'status'];
+  currentPage = 0;
   ngOnInit() {
 
-  this.getBugsService.getBugs(null, null, null, null, null, null, null, null).subscribe(
+  this.getBugsService.getBugs('createdAt', 0, null, null, null, null, null, null).subscribe(
     (bugs: Bug[]) => {this.bugs = bugs;});
   }
 
   sorting( sortHead: string) {
-    this.getBugsService.getBugs(sortHead, null, null, null, null, null, null, null).subscribe(
+    this.currentPage = 0;
+    this.getBugsService.getBugs(sortHead, 0, null, null, null, null, null, null).subscribe(
+      (bugs: Bug[]) => {this.bugs = bugs;});
+  }
+
+  paging( goTo: string) {
+    this.currentPage = goTo === 'next'? this.currentPage+1 : this.currentPage-1 ;
+    this.getBugsService.getBugs(null, this.currentPage, null, null, null, null, null, null).subscribe(
       (bugs: Bug[]) => {this.bugs = bugs;});
   }
 
