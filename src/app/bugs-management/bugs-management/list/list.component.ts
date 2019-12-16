3cd8a0e34258dca36cd4ef4d0c9376b72f334db3
oxana-l7 <58442303+oxana-l7@users.noreply.github.com>
2019-12-16 18:03:22 +0200

    paging

--- src/app/bugs-management/bugs-management/list/list.component.ts
+++ src/app/bugs-management/bugs-management/list/list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { GetBugsService } from './../../../services/get-bugs.service';
 import { Bug } from 'src/app/app.component';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-list',
@@ -10,31 +11,36 @@ import { Bug } from 'src/app/app.component';
 
 export class ListComponent implements OnInit {
 
-  constructor(private getBugsService: GetBugsService) { }
+  constructor(private getBugsService: GetBugsService, private http: HttpClient) { }
   public bugs: Bug[];
   headElements = ['title', 'priority', 'reporter', 'createdAt', 'status'];
   currentPage = 0;
-
+res;
+totalPages;
+//headers: Headers;
   ngOnInit() {
     this.loadList();
   }
 
   loadList(){
-  this.getBugsService.getBugs('createdAt', 0, null, null, null, null, null, null).subscribe(
-    (bugs: Bug[]) => {this.bugs = bugs;});
-  }
+    this.getBugsService.getBugs('createdAt', 0, null, null, null, null, null, null)
+    .subscribe( res => {
+      this.bugs = res.body as Bug[];
+      this.totalPages = res.headers.get('Totalpages');
+    })}
 
   sorting( sortHead: string) {
     this.currentPage = 0;
     this.getBugsService.getBugs(sortHead, 0, null, null, null, null, null, null).subscribe(
-      (bugs: Bug[]) => {this.bugs = bugs;});
-  }
+      res => { this.bugs = res.body as Bug[];
+        this.totalPages = res.headers.get('Totalpages');})}
+
 
   paging( goTo: string) {
     this.currentPage = goTo === 'next'? this.currentPage+1 : this.currentPage-1 ;
     this.getBugsService.getBugs(null, this.currentPage, null, null, null, null, null, null).subscribe(
-      (bugs: Bug[]) => {this.bugs = bugs;});
-  }
+      res => { this.bugs = res.body as Bug[];
+        this.totalPages = res.headers.get('Totalpages');})}
 
 }
 
