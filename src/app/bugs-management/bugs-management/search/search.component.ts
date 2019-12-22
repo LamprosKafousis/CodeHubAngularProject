6b31cd9147dee378fe910ef2a46da02bcf53279b
oxana-l7 <58442303+oxana-l7@users.noreply.github.com>
2019-12-22 14:32:05 +0200

    search vol1

--- src/app/bugs-management/bugs-management/search/search.component.ts
+++ src/app/bugs-management/bugs-management/search/search.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
-
+  searchForm: FormGroup;
+  priorities = ['minor','major', 'critical'];
+  reporters = ['QA', 'PO', 'DEV'];
+  statuses = ['Ready for test', 'Done', 'Rejected'];
   constructor() { }
 
   ngOnInit() {
+    this.searchForm = new FormGroup({
+      title: new FormControl('', Validators.required),
+      priority: new FormControl('', Validators.required),
+      reporter: new FormControl('', Validators.required),
+      status: new FormControl('', Validators.required),
+    });
   }
 
 }
