4cc801ad9072920366da546fccf2106c7d3d1b38
oxana-l7 <58442303+oxana-l7@users.noreply.github.com>
2019-12-26 17:27:56 +0200

    search vol2

--- src/app/bugs-management/bugs-management/search/search.component.ts
+++ src/app/bugs-management/bugs-management/search/search.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { GetBugsService } from 'src/app/services/get-bugs.service';
+import { Bug } from 'src/app/app.component';
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
@@ -7,18 +9,33 @@ import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 })
 export class SearchComponent implements OnInit {
   searchForm: FormGroup;
-  priorities = ['minor','major', 'critical'];
+  priorities = [1,2,3];
   reporters = ['QA', 'PO', 'DEV'];
   statuses = ['Ready for test', 'Done', 'Rejected'];
-  constructor() { }
+  constructor(private getBugsService: GetBugsService,) {
 
+   }
+   public bugs: Bug[];
+   headElements = ['title', 'priority', 'reporter', 'createdAt', 'status'];
+   currentPage = 0;
+ res;
+ totalPages;
   ngOnInit() {
     this.searchForm = new FormGroup({
-      title: new FormControl('', Validators.required),
-      priority: new FormControl('', Validators.required),
-      reporter: new FormControl('', Validators.required),
-      status: new FormControl('', Validators.required),
+      title: new FormControl(''),
+      priority: new FormControl(''),
+      reporter: new FormControl(''),
+      status: new FormControl(''),
     });
   }
+
+  search() {
+
+    this.getBugsService.getBugs('createdAt', 0, null, null, this.searchForm.controls['priority'].value, this.searchForm.controls['reporter'].value, this.searchForm.controls['status'].value, null)
+    .subscribe( res => {
+      this.bugs = res.body as Bug[];
+      this.totalPages = res.headers.get('Totalpages');
+      console.log(this.bugs);
+    })}
 
 }
