9fe067259c75f3968f9a8c5314d7d7fa9d9cc67e
oxana-l7 <58442303+oxana-l7@users.noreply.github.com>
2019-12-29 00:55:05 +0200

    search comp into list comp

--- src/app/bugs-management/bugs-management/list/list.component.ts
+++ src/app/bugs-management/bugs-management/list/list.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { GetBugsService } from './../../../services/get-bugs.service';
 import { Bug } from 'src/app/app.component';
 import { DeleteBugsService } from 'src/app/services/delete-bugs.service';
-
+import { FormGroup, FormControl } from '@angular/forms';
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
@@ -12,42 +12,72 @@ import { DeleteBugsService } from 'src/app/services/delete-bugs.service';
 export class ListComponent implements OnInit {
 
   constructor(private getBugsService: GetBugsService, private deleteBugsService: DeleteBugsService) { }
-  public bugs: Bug[];
+  bugs: Bug[];
   headElements = ['title', 'priority', 'reporter', 'createdAt', 'status'];
   currentPage = 0;
-res;
-totalPages;
-//headers: Headers;
+  res;
+  totalPages;
+  searchForm: FormGroup;
+  priorities = [1,2,3];
+  reporters = ['QA', 'PO', 'DEV'];
+  statuses = ['Ready for test', 'Done', 'Rejected'];
+  sortHead;
   ngOnInit() {
     this.loadList();
+    this.searchForm = new FormGroup({
+      title: new FormControl(''),
+      priority: new FormControl(''),
+      reporter: new FormControl(''),
+      status: new FormControl(''),
+    });
   }
 
-  loadList(){
+  loadList() {
     this.getBugsService.getBugs('createdAt', 0, null, null, null, null, null, null)
-    .subscribe( res => {
-      this.bugs = res.body as Bug[];
-      this.totalPages = res.headers.get('Totalpages');
-    })}
+    .subscribe( res => this.getBugsList(res)); }
 
   sorting( sortHead: string) {
-    this.currentPage = 0;
-    this.getBugsService.getBugs(sortHead, 0, null, null, null, null, null, null).subscribe(
-      res => { this.bugs = res.body as Bug[];
-        this.totalPages = res.headers.get('Totalpages');})}
-
+    this.sortHead = sortHead;
+    this.currentPage = 0 ;
+    this.getBugsService.getBugs(sortHead, 0, null,
+      this.searchForm.controls['title'].value,
+      this.searchForm.controls['priority'].value,
+      this.searchForm.controls['reporter'].value,
+      this.searchForm.controls['status'].value,
+      null)
+    .subscribe(  res => this.getBugsList(res)); }
 
   paging( goTo: string) {
-    this.currentPage = goTo === 'next'? this.currentPage+1 : this.currentPage-1 ;
-    this.getBugsService.getBugs(null, this.currentPage, null, null, null, null, null, null).subscribe(
-      res => { this.bugs = res.body as Bug[];
-        this.totalPages = res.headers.get('Totalpages');})}
+    this.currentPage = goTo === 'next' ? this.currentPage + 1 : this.currentPage - 1 ;
+    this.getBugsService.getBugs(null, this.currentPage, null,
+    this.searchForm.controls['title'].value,
+    this.searchForm.controls['priority'].value,
+    this.searchForm.controls['reporter'].value,
+    this.searchForm.controls['status'].value,
+    null)
+    .subscribe( res => this.getBugsList(res)); }
 
   delete( bugId: string) {
+    if ( confirm('Are you sure to delete bug with id: ' + bugId)) {
     this.deleteBugsService.deleteBugs(bugId).subscribe();
-    this.loadList();
+    this.loadList(); }
   }
-}
+
+  getBugsList( res ) {
+    this.bugs = res.body as Bug[];
+    this.totalPages = res.headers.get('Totalpages');
+  }
+
+  search() {
+    this.currentPage = 0 ;
+    this.getBugsService.getBugs(null, 0, null,
+      this.searchForm.controls['title'].value,
+      this.searchForm.controls['priority'].value,
+      this.searchForm.controls['reporter'].value,
+      this.searchForm.controls['status'].value,
+       null)
+    .subscribe( res => {
+      this.getBugsList(res);})}
 
 
-
-
+  }
